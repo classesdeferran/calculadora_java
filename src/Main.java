375dfbffa79a75e506cb641dba255a420b2903bb
Ferran <ferran.docencia@gmail.com>
2023-11-22 11:13:14 +0100

    calculadora v1

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,48 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
+        Scanner input = new Scanner(System.in);
+
         System.out.println("Mi calculadora!");
 
-        Scanner input = new Scanner(System.in);
+        System.out.println("¿Cómo te llamas?");
+        String nombre = input.next();
+
+        System.out.println(STR."Te llamas \{nombre}");
+
+        try {
+
+            calcular(input, nombre);
+
+        } catch (Exception e) {
+
+            System.out.println("Error en los valores");
+            // System.out.println("Exception : " + e);
+        }
+
+
+
+
+    input.close();
+    }
+
+    public static Boolean comprobarDivisor(double divisor){
+        if (divisor == 0) {
+            System.out.println("No se puede dividir por cero");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public static void calcular(Scanner input, String nombre) {
+
         System.out.println("Dime el primer número");
         double num1 = input.nextDouble();
         System.out.println("Dime el segundo número");
         double num2 = input.nextDouble();
-
-        String menu = """
+        String menu = STR."""
+                Tu calculadora \{nombre}
                 1 - suma
                 2 - resta
                 3 - multiplicación
@@ -36,7 +69,9 @@ public class Main {
                 System.out.println("La multiplicación es " + (num1 * num2));
                 break;
             case 4:
-                System.out.println("La división es " + (Math.round(100.0 * (num1 / num2)))/100.0);
+                if (comprobarDivisor(num2)) {
+                    System.out.println("La división es " + Math.round(100.0 * (num1 / num2))/100.0);
+                }
                 break;
             case 5:
                 System.out.println("La raíz cuadrada es " + Math.sqrt(num1));
@@ -46,14 +81,7 @@ public class Main {
 
         }
 
-
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
     }
+
+
 }
